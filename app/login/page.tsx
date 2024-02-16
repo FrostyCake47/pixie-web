3f636634435a2f157c0dfc1185bfb6d0a78218c3
Akash <AwesomeAkash47@gmail.com>
2024-02-16 23:16:27 +0530

    login styled

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from 'react';
+import Link from 'next/link';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -9,11 +10,22 @@ const Login = () => {
     <div className='login'>
       <form>
         <p className="login-intro">Welcome back!</p>
-        <label htmlFor='email'>Email</label>
-        <input type="email" className='email' value={email} onChange={(e)=>{setEmail(e.target.value)}} required/>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input type="email" className='email' value={email} onChange={(e)=>{setEmail(e.target.value)}} required/>
+        </div>
 
-        <label htmlFor='pass'>Password</label>
-        <input type="pass" className='pass' value={pass} onChange={(e)=>{setPass(e.target.value)}} required/>
+        <div>
+          <label htmlFor='pass'>Password</label>
+          <input type="pass" className='pass' value={pass} onChange={(e)=>{setPass(e.target.value)}} required/>
+
+          <div className='form-links'>
+            <Link href='/register' className='form-link'>Register</Link>
+            <Link href='/forgotpass' className='form-link'>Forgot password?</Link>
+          </div>
+        </div>
+
+        
 
         <button className='login-btn'>Login</button>
       </form>
