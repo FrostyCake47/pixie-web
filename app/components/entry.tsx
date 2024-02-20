acc5dbdf4bd937900bd7647792cc21926576c7f6
Akash <AwesomeAkash47@gmail.com>
2024-02-20 23:59:31 +0530

    looks good

--- app/components/entry.tsx
+++ app/components/entry.tsx
@@ -4,6 +4,8 @@ const Entry = () => {
   return (
     <div className='flex flex-col px-10 py-8'>
         <h1 className='text-white text-[30px]'>Small Memories</h1>
+        <p className='text-neutral-400'>5 Wed | 2:30pm</p>
+        <p className='text-neutral-200 py-[30px]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto rem impedit delectus laborum! Expedita, neque nobis sapiente voluptatem quam molestiae tenetur, omnis recusandae nesciunt similique ullam perspiciatis adipisci fuga quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime ipsum dolorum, nam libero ducimus ut, eum quidem est corrupti, laudantium maiores. Eveniet eligendi non sequi veniam totam nemo fuga.</p>
     </div>
   )
 }
