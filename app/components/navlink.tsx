2ef39d02168343b7fd472bb4999d9ce86c6b02ea
Akash <AwesomeAkash47@gmail.com>
2024-03-02 15:47:58 +0530

    changed colours of navlink

--- app/components/navlink.tsx
+++ app/components/navlink.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const NavLink = (props: {href : string, text: string}) => {
     const {href, text} = props;
   return (
-    <Link href={href} className="flex flex-row mx-2 py-1 my-3 rounded-[10px] items-center justify-center px-5 bg-gradient-to-r from-zinc-500 to-zinc-600 shadow-lg shadow-neutral-700/50 transition-all ease-in-out hover:from-zinc-400 hover:to-zinc-600 duration-300 cursor-pointer">{text}</Link>
+    <Link href={href} className="flex flex-row mx-2 py-1 my-3 rounded-[10px] items-center justify-center px-5 bg-gradient-to-r from-neutral-900 to-neutral-900 hover:shadow-lg shadow-neutral-700/50 transition-all ease-in-out hover:from-zinc-600 hover:to-zinc-900 duration-300 cursor-pointer">{text}</Link>
   )
 }
 
